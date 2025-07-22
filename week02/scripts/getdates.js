5d1ef33bf1fef01a67cf07fb11252f117d6bc4f6
emm1988 <ejovortuo@byupathway.edu>
2025-07-22 16:19:19 -0400

    track

---
+++ week02/scripts/getdates.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const currentYearElement = document.getElementById("currentyear");
+  if (currentYearElement) {
+    currentYearElement.textContent = new Date().getFullYear();
+  }
+
+ 
+  const lastUpdatedElement = document.getElementById("last-updated-date");
+  if (lastUpdatedElement) {
+    lastUpdatedElement.textContent = document.lastModified;
+  }
+});
